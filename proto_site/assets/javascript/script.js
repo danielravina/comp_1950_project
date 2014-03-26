8e705fb44876f18313365c9c1c1cd7c76246e283
DanielRavina <raabhim@gmail.com>
2014-03-25 21:34:18 -0700

    add jquery smoooooth scroll

--- proto_site/assets/javascript/script.js
+++ proto_site/assets/javascript/script.js
@@ -1,1 +1,8 @@
 // Add scroll to here
+$(document).ready(function(){
+  $("#tabs a").click (function(e){
+    e.preventDefault()
+    target = e.target.getAttribute('href');
+    $(".content-box").scrollTo(target, 1000)
+  })
+});
